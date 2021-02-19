24f258062d4e3268aae96a5d85ec6a64f44ae66c
sagar797 <gupta.sagar797@gmail.com>
2021-02-19 16:57:49 +0530

    modal window completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//document.querySelector('.modal').classList.remove('hidden');
+let modal=document.querySelector('.modal');
+console.log(modal);
+let showModalBtn=document.querySelectorAll('.show-modal');
+console.log(showModalBtn);
+let overlay=document.querySelector('.overlay');
+
+for(let i=0;i<showModalBtn.length;i++){
+    document.querySelector(showModalBtn[i].addEventListener('click',function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    }));
+}
+
+let closeModal=document.querySelector('.close-modal');
+if(!closeModal.classList.contains('hidden')){
+    closeModal.addEventListener('click',function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+}
