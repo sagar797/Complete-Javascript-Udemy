014bae404d77c1f9a0dd6d1aa00ce5159685c1b2
sagar797 <gupta.sagar797@gmail.com>
2021-02-19 17:03:28 +0530

    updated

---
+++ 03-Developer-Skills/script.js
@@ -0,0 +1,9 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// const arr=[17,21,23];
+const arr=[12, 5, -5, 0, 4];
+
+for(let i=0;i<arr.length;i++){
+    console.log(`${arr[i]} in ${i+1} days...`);
+}
