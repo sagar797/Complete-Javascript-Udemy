0565b72e134e1939662cea40d66ead7cc6ba8268
sagar797 <gupta.sagar797@gmail.com>
2021-02-20 19:38:52 +0530

    closure

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// double arrow function
+// const greet=greeting=> name=> console.log(`${greeting} ${name}`);
+// greet('Hey')('Sagar!');
+
+
+// closure
+let fn=function(){
+    let a=0;
+    return function(){
+        a++;
+        console.log(a);
+    }
+}
+
+const func=fn();
+console.dir(func);
+func();
+func();
+func();
